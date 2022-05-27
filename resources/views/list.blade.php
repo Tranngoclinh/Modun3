f423bec3f935db7afc1cdb9e7211293f352ad30c
Tranngoclinh <nguyenlinh23444@gmail.com>
2022-05-27 16:16:18 +0700

    bai Task Management

---
+++ resources/views/list.blade.php
@@ -0,0 +1,51 @@
+@extends('index')
+@section('title', 'Danh sách khách hàng')
+@section('content')
+    <div class="col-12">
+        <div class="row">
+            <div class="col-12"><h1>Danh Sách Khách Hàng</h1></div>
+            <div class="col-12">
+                @if (Session::has('success'))
+                    <p class="text-success">
+                        <i class="fa fa-check" aria-hidden="true"></i>{{ Session::get('success') }}
+                    </p>
+                @endif
+            </div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tên khách hàng</th>
+                    <th scope="col">Ngày Sinh</th>
+                    <th scope="col">Email</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tr>
+                   <td>1</td>
+                   <td>Linh</td>
+                   <td>01/11/2003</td>
+                   <td>nguyenlinh23444@gmail.com</td>
+                </tr>
+                {{-- <tbody>
+                @if(count($customers) == 0)
+                    <tr><td colspan="4">Không có dữ liệu</td></tr>
+                @else
+                    @foreach($customers as $key => $customer)
+                        <tr>
+                            <th scope="row">{{ ++$key }}</th>
+                            <td>{{ $customer->name }}</td>
+                            <td>{{ $customer->dob }}</td>
+                            <td>{{ $customer->email }}</td>
+                            <td><a href="{{ route('Demo.edit', $customer->id) }}">sửa</a></td>
+                            <td><a href="{{ route('Demo.destroy', $customer->id) }}" class="text-danger" onclick="return confirm('Bạn chắc chắn muốn xóa?')">xóa</a></td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody> --}}
+            </table>
+            <a class="btn btn-primary" href="{{ route('Demo.create') }}">Thêm mới</a>
+        </div>
+    </div>
+@endsection
